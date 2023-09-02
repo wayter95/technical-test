0b4f483f424049f6a54794233d39ad6a8ce2de74
Wayter Paulo <wayter.paulo.95@gmail.com>
2023-09-02 17:47:48 -0300

    feat: create product repository

---
+++ backend/src/infra/database/repositories/product-repository.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@nestjs/common";
+import { Product } from "src/domain/entities/product.entity";
+
+@Injectable()
+export abstract class ProductRepository {
+  abstract list(): Promise<Product[] | []>
+  abstract findById(): Promise<Product>
+}
