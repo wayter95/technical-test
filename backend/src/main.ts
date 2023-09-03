a68dadd06348be79efd617349117c38faff324e0
Wayter Paulo <wayter.paulo.95@gmail.com>
2023-09-03 15:43:52 -0300

    feat: add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders:
+      'Content-Type,Accept,Authorization,Access-Control-Allow-Origin',
+  });
+  
   await app.listen(3000);
 }
 bootstrap();
