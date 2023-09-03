ce3c982f0c6bbf63d39790f9cbf386c1e51b2554
Wayter Paulo <wayter.paulo.95@gmail.com>
2023-09-03 16:38:35 -0300

    feat: should throw 409 if product does not exist

--- backend/src/application/use-cases/product/find-product-by-id.use-case.ts
+++ backend/src/application/use-cases/product/find-product-by-id.use-case.ts
@@ -1,14 +1,20 @@
 import { Injectable } from "@nestjs/common";
 import { Product } from "src/domain/entities/product.entity";
 import { ProductRepository } from "src/infra/database/repositories/product-repository";
+import { ProductByIdNotFoundError } from "./errors/product-not-found.error";
 
+type FindProductByIdUseCaseResponse = Product | ProductByIdNotFoundError;
 
 @Injectable()
 export class FindProductByIdUseCase {
-  constructor(private readonly productRepository: ProductRepository) {}
-  
-  async handle(id: string): Promise<Product> {
+  constructor(private readonly productRepository: ProductRepository) { }
+
+  async handle(id: string): Promise<FindProductByIdUseCaseResponse> {
     const product = await this.productRepository.findById(id);
+
+    if (!product) {
+      throw new ProductByIdNotFoundError()
+    }
 
     return product;
   }
