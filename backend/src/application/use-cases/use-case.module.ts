edf2acfac705d49787bf713605e2c9c17fd24ed3
Wayter Paulo <wayter.paulo.95@gmail.com>
2023-09-03 17:02:53 -0300

    feat: create find by product route

--- backend/src/application/use-cases/use-case.module.ts
+++ backend/src/application/use-cases/use-case.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "src/infra/database/database.module";
 import { ListProductUseCase } from "./product/list-product.use-case";
+import { FindProductByIdUseCase } from "./product/find-product-by-id.use-case";
 
 @Module({
   imports: [DatabaseModule],
-  providers: [ListProductUseCase],
-  exports: [ListProductUseCase],
+  providers: [ListProductUseCase, FindProductByIdUseCase],
+  exports: [ListProductUseCase, FindProductByIdUseCase],
 })
 
 export class UseCaseModule {};
--- backend/src/infra/http/controllers/product.controller.ts
+++ backend/src/infra/http/controllers/product.controller.ts
@@ -1,17 +1,57 @@
-import { Controller, Get, Res } from "@nestjs/common";
+import { Controller, Get, Param, Res } from "@nestjs/common";
 import { ApiOkResponse, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Response } from "express";
+import { InactiveProductError } from "src/application/use-cases/product/errors/inactive-product";
+import { ProductByIdNotFoundError } from "src/application/use-cases/product/errors/product-not-found.error";
+import { FindProductByIdUseCase } from "src/application/use-cases/product/find-product-by-id.use-case";
 import { ListProductUseCase } from "src/application/use-cases/product/list-product.use-case";
 import { Product } from "src/domain/entities/product.entity";
 
 @ApiTags("Produto")
 @Controller('product')
 export class ProductController {
-  constructor(private readonly listProductUseCase: ListProductUseCase) { }
+  constructor(
+    private readonly listProductUseCase: ListProductUseCase,
+    private readonly findProductByIdUseCase: FindProductByIdUseCase
+  ) { }
+
+
+  @Get(":id")
+  @ApiOkResponse({ status: 200, description: "Listar produtos ativos", type: Product })
+  @ApiResponse({ status: 500, description: "Erro ao executar" })
+  async findById(
+    @Param('id') id: string,
+    @Res() response: Response,
+  ) {
+    try {
+      const result = await this.findProductByIdUseCase.handle(id);
+
+      return response.status(200).json({
+        success: true,
+        message: "Server error",
+        data: result
+      })
+    } catch (error) {
+      if (
+        error instanceof ProductByIdNotFoundError ||
+        error instanceof InactiveProductError) {
+        return response.status(error.statusCode).json({
+          success: false,
+          message: error.message
+        });
+      }
+
+      return response.status(500).json({
+        success: false,
+        message: "Server error",
+        error
+      });
+    }
+  }
 
   @Get("")
-  @ApiOkResponse({isArray: true, status: 200, description: "Listar produtos ativos", type: Product })
-  @ApiResponse({status: 500, description: "Erro ao executar"})
+  @ApiOkResponse({ isArray: true, status: 200, description: "Listar produtos ativos", type: Product })
+  @ApiResponse({ status: 500, description: "Erro ao executar" })
   async list(
     @Res() response: Response,
   ): Promise<Response> {
