689607a97017934d74606370673c11c7161310fe
Wayter Paulo <wayter.paulo.95@gmail.com>
2023-09-03 16:56:49 -0300

    feat: should throw 400 if product is not active

--- backend/src/application/use-cases/product/find-product-by-id.use-case.ts
+++ backend/src/application/use-cases/product/find-product-by-id.use-case.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { Product } from "src/domain/entities/product.entity";
 import { ProductRepository } from "src/infra/database/repositories/product-repository";
 import { ProductByIdNotFoundError } from "./errors/product-not-found.error";
+import { InactiveProductError } from "./errors/inactive-product";
 
 type FindProductByIdUseCaseResponse = Product | ProductByIdNotFoundError;
 
@@ -13,7 +14,11 @@ export class FindProductByIdUseCase {
     const product = await this.productRepository.findById(id);
 
     if (!product) {
-      throw new ProductByIdNotFoundError()
+      throw new ProductByIdNotFoundError();
+    }
+
+    if(!product.isActive) {
+      throw new InactiveProductError();
     }
 
     return product;
