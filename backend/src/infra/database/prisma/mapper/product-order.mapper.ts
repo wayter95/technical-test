aef48f39a219523c475eadb784ca13c6cac1b164
Wayter Paulo <wayter.paulo.95@gmail.com>
2023-09-04 01:56:36 -0300

    feat: create product order mapper

---
+++ backend/src/infra/database/prisma/mapper/product-order.mapper.ts
@@ -0,0 +1,29 @@
+import { Prisma, ProductOrder as RawProductOrder } from '@prisma/client';
+import { ProductOrder } from 'src/domain/entities/product-order.entity';
+
+export class OrderMapper {
+  static toDomain(raw: RawProductOrder): ProductOrder {
+    const productOrder = new ProductOrder({
+      orderId: raw.orderId,
+      productId: raw.productId,
+    }, raw.id)
+
+    return productOrder;
+  }
+
+  static toPersistence(productOrder: ProductOrder): Prisma.ProductOrderCreateInput {
+    return {
+      id: productOrder.id,
+      order: {
+        connect: {
+          id: productOrder.orderId
+        }
+      },
+      product: {
+        connect: {
+          id: productOrder.productId
+        }
+      }
+    }
+  }
+}
