ca265a5b43cc57566e8dfcd43b2fb82c4e0fa802
Wayter Paulo <wayter.paulo.95@gmail.com>
2023-09-04 11:53:01 -0300

    feat: should return successfully for create order

--- backend/src/application/use-cases/order/create-order.use-case.ts
+++ backend/src/application/use-cases/order/create-order.use-case.ts
@@ -4,6 +4,7 @@ import { OrderRepository } from "src/infra/database/repositories/order.repositor
 import { ProductOrderRepository } from "src/infra/database/repositories/product-order.repository";
 import { ProductRepository } from "src/infra/database/repositories/product-repository";
 import { ProductByIdNotFoundError } from "./errors/product-by-id-not-found.error";
+import { ProductOrder } from "src/domain/entities/product-order.entity";
 
 type ProductOrderProps = {
   amount: number;
@@ -26,17 +27,22 @@ export class CreateOrderUseCase {
   ) { }
 
   async handle(request: CreateOrderUseCaseRequest): Promise<CreateOrderUseCaseResponse> {
-
     const newOrder = new Order(request)
 
     const order = await this.orderRepository.create(newOrder);
 
-    for(const product of request.products) {
+    for (const product of request.products) {
       const productExist = await this.productRepository.findById(product.id);
 
-      if(!productExist) {
+      if (!productExist) {
         throw new ProductByIdNotFoundError();
       }
+    }
+
+    for (const product of request.products) {
+      const productOrder = new ProductOrder({ amount: product.amount, productId: product.id, orderId: order.id })
+
+      await this.productOrderRepository.create(productOrder);
     }
 
     return order;
