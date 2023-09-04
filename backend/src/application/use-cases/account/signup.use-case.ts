8200be7671bcaf9d837bfaf2b3d6f91797d612ec
Wayter Paulo <wayter.paulo.95@gmail.com>
2023-09-03 23:35:37 -0300

    feat: should successfully register account

--- backend/src/application/use-cases/account/signup.use-case.ts
+++ backend/src/application/use-cases/account/signup.use-case.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { AccountRepository } from "src/infra/database/repositories/account-repository";
 import { AccountByEmailAlreadyExistError } from "./errors/account-by-email-already-exist.error";
+import { hash } from 'bcrypt';
+import { Account } from "src/domain/entities/account.entity";
 
 type SignUpUseCaseRequest = {
   fullName: string;
@@ -13,11 +15,19 @@ type SignUpUseCaseRequest = {
 export class SignUpUseCase {
   constructor (private readonly accountRepository: AccountRepository) {}
   
-  async handle(request: SignUpUseCaseRequest): Promise<void> {
+  async handle(request: SignUpUseCaseRequest): Promise<Account> {
     const accountAlreadyExist = await this.accountRepository.findByEmail(request.email);
 
     if(accountAlreadyExist) {
       throw new AccountByEmailAlreadyExistError();
     }
+
+    const hashedPassword = await hash(request.password, 12);
+
+    const newAccount = new Account({...request, password: hashedPassword});
+
+    const account = await this.accountRepository.create(newAccount);
+
+    return account;
   }
 }
