b0a1a205eb2bf90271df4b825c83b8363abdbad3
Wayter Paulo <wayter.paulo.95@gmail.com>
2023-09-05 15:11:12 -0300

    fix: change api env

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const url = process.env.REACT_APP_API;
+const url = process.env.NEXT_PUBLIC_API_URL;
 
 export const api = axios.create({
-  baseURL: `${url}/api`,
+  baseURL: `${url}`,
 });
