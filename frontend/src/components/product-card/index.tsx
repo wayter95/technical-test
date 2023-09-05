ae4ffdb60178dc37827f5b19506de73dc0dca799
Wayter Paulo <wayter.paulo.95@gmail.com>
2023-09-05 17:07:12 -0300

    fix: change colocar product card route

--- frontend/src/components/product-card/index.tsx
+++ frontend/src/components/product-card/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "../button";
 import styles from "./styles.module.css";
 import { formatToBRL } from "@/utils/format-to-brl";
+import { useRouter } from 'next/navigation'
 
 interface IProductCardProps {
   id: string;
@@ -10,10 +11,12 @@ interface IProductCardProps {
 }
 
 const ProductCard = ({ id, name, photoUrl, price }: IProductCardProps) => {
+  const router = useRouter()
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} >
       <div className={styles.content}>
-        <img alt={name} src={photoUrl} width={300} height={600} />
+        <img alt={name} src={photoUrl} width={300} height={600} onClick={() => router.push(`/product/${id}`, { scroll: false })}/>
         <span>{name}</span>
         <h2>{formatToBRL(price)}</h2>
         <span>6x de {formatToBRL(price / 6)} s/ juros</span>
