93b5111b1d7f8a34250ffa2c20a7927c416920dd
Wayter Paulo <wayter.paulo.95@gmail.com>
2023-09-05 15:11:45 -0300

    feat: create home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,45 @@
-import "../styles/globals.css";
+"use client";
+
+import Image from "next/image";
+import styles from "../styles/home.module.css";
+import { ProductCard } from "@/components/product-card";
+import { useEffect, useState } from "react";
+import { api } from "@/services/api";
+
 
 export default function Home() {
+  const [products, setProducts] = useState<IProductModel[]>([])
+
+  useEffect(() => {
+    api.get("/product").then((response) => {
+      setProducts(response.data.data);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }, [])
+
   return (
-    <h1>Clean....</h1>
+    <div className={styles.container}>
+      <div className={styles.banner}>
+        <Image src="/images/banner.svg" width={1920} height={400} layout="responsive" alt="Technical Test" />
+      </div>
+
+      <div className={styles.content}>
+        {
+          products.length ? 
+          products.map((product) => (
+            <ProductCard
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              photoUrl={product.photoUrl}
+              price={product.value}
+            />
+          ))
+          : 
+          <span className={styles.alert}>Nenhum produto encontrado</span>
+        }
+      </div>
+    </div>
   )
 }
