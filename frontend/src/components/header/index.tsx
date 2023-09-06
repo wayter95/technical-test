3595e38c2af902b45be432dd68cbe43fe5533ccb
Wayter Paulo <wayter.paulo.95@gmail.com>
2023-09-05 23:49:59 -0300

    feat: add modal register in to header button

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -4,9 +4,13 @@ import Image from "next/image"
 import Link from "next/link"
 import styles from './styles.module.css'
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Modal } from "../modal";
+import { Forms } from "../forms";
 
 const Header = () => {
   const route = useRouter();
+  const [openModal, setOpenModal] = useState(false);
 
   return (
     <header>
@@ -22,12 +26,20 @@ const Header = () => {
             <Image src="/icons/outline-box.svg" width={20} height={20} alt="Technical Test" />
             <span>Carrinho</span>
           </button>
-          <button type="button" className={styles.btnHeader}>
+          <button type="button" className={styles.btnHeader} onClick={() => setOpenModal(true)}>
             <Image src="/icons/outline-user.svg" width={20} height={20} alt="Technical Test" />
             <span>Entrar</span>
           </button>
         </div>
       </div>
+      <Modal
+        isOpen={openModal}
+        onRequestClose={() => setOpenModal(false)}
+        iconPath="/icons/outline-user.svg"
+        title="Entrar"
+      >
+        <Forms />
+      </Modal>
     </header>
   )
 }
