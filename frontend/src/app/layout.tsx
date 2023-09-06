cf84dccdbe00b9e2e2bed0f2cc32c69a787784bb
Wayter Paulo <wayter.paulo.95@gmail.com>
2023-09-06 01:08:30 -0300

    feat: add toast

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,13 @@
-import "../styles/globals.css";
-
+import { AuthProvider } from "@/contexts/auth-context";
 import { Header } from '@/components/header'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const inter = Poppins({weight: ["400", "500", "600", "700" ], subsets: ['latin'] })
+import "../styles/globals.css";
+
+const inter = Poppins({ weight: ["400", "500", "600", "700"], subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Technical Test',
@@ -19,10 +22,13 @@ export default function RootLayout({
   return (
     <html lang="pt">
       <body className={inter.className}>
-        <>
-          <Header />
-          {children}
-        </>
+        <AuthProvider >
+          <>
+            <Header />
+            {children}
+            <ToastContainer position="top-right" autoClose={5000} />
+          </>
+        </AuthProvider>
       </body>
     </html>
   )
