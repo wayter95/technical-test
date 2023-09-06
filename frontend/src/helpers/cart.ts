3ea3866a0454a3fed49e043cd9d1f762055d6f76
Wayter Paulo <wayter.paulo.95@gmail.com>
2023-09-06 01:47:07 -0300

    fix: change name cart in to local storage

--- frontend/src/helpers/cart.ts
+++ frontend/src/helpers/cart.ts
@@ -13,8 +13,8 @@ export interface IProductCartModel {
 }
 
 export const addToCart = (product: IProductCartModel) => {
-  const cart = JSON.parse(localStorage.getItem('cart') || '[]');
-  console.log("cart", cart);
+  const cart = JSON.parse(localStorage.getItem('@TECHNICALTEST:cart') || '[]');
+
   const foundProduct = cart.find((item: IProductCartModel) => item.id === product.id);
   if (foundProduct) {
     foundProduct.amount += 1;
@@ -22,24 +22,24 @@ export const addToCart = (product: IProductCartModel) => {
     product.amount = 1;
     cart.push(product);
   }
-  localStorage.setItem('cart', JSON.stringify(cart));
+  localStorage.setItem('@TECHNICALTEST:cart', JSON.stringify(cart));
 };
 
 export const removeFromCart = (productId: string) => {
-  const cart = JSON.parse(localStorage.getItem('cart') || '[]');
+  const cart = JSON.parse(localStorage.getItem('@TECHNICALTEST:cart') || '[]');
   const newCart = cart.filter((item: IProductCartModel) => item.id !== productId);
-  localStorage.setItem('cart', JSON.stringify(newCart));
+  localStorage.setItem('@TECHNICALTEST:cart', JSON.stringify(newCart));
 };
 
 export const updateProductAmount = (productId: string, quantity: number) => {
-  const cart = JSON.parse(localStorage.getItem('cart') || '[]');
+  const cart = JSON.parse(localStorage.getItem('@TECHNICALTEST:cart') || '[]');
   const product = cart.find((item: IProductCartModel) => item.id === productId);
   if (product) {
     product.amount = quantity;
-    localStorage.setItem('cart', JSON.stringify(cart));
+    localStorage.setItem('@TECHNICALTEST:cart', JSON.stringify(cart));
   }
 };
 
 export const getCart = (): IProductCartModel[] => {
-  return JSON.parse(localStorage.getItem('cart') || '[]');
+  return JSON.parse(localStorage.getItem('@TECHNICALTEST:cart') || '[]');
 };
